a0f4abc2544c98640cb3a0c38e372f043c5d4c5a
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-07-14 17:07:33 +0500

    code-update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import {
     Route,
     Routes
 } from 'react-router-dom';
+import './styles/App.scss';
+import './styles/Header.scss';
+import './styles/Home.scss';
+import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />}/>
       </Routes>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,69 @@
+import React from 'react'
+import vg from "../assets/2.webp";
+import {
+    AiFillGoogleCircle,
+    AiFillAmazonCircle,
+    AiFillYoutube,
+    AiFillInstagram
+} from 'react-icons/ai';
 const Home = () => {
-  return (
-    <div >
-        <p>I'm from Home.</p>
-    </div>
-  )
+    return (
+        <>
+            <div className="home">
+                <main>
+                    <h1>TechStar</h1>
+                    <p>Your problems need a digital Solution.</p>
+                </main>
+            </div>
+
+            <div className="home2">
+                <img src={vg} alt="Graphics" />
+                <div>
+                    <p>
+                        We are your one and only solution to the tech problems you face every day. We are leading tech company whose aim is to increase solving ability in children.
+                    </p>
+                </div>
+            </div>
+
+
+            <div className="home3">
+                <div>
+                    <h1>Who I am?</h1>
+                    <p>As a front-end web developer, I am passionate about creating attractive and visually appealing websites that offer an exceptional user experience. I am skillful in HTML, CSS, and JavaScript, with a good understanding of the principles of responsive web design. I have experience with modern front-facing frameworks like React and Angular. I also know how to work with various design tools like Sketch and Figma to create wireframes and prototypes. With my excellent communication skills and attention to detail, I am committed to high-quality work that exceeds client expectations.</p>
+                </div>
+            </div>
+
+            <div className="home4">
+                <h1>Brands</h1>
+                <article>
+                    <div style={{
+                        animationDelay: "0.3s",
+                    }}>
+                        <AiFillGoogleCircle />
+                        <p>Google</p>
+                    </div>
+                    <div style={{
+                        animationDelay: "0.5s",
+                    }}>
+                        <AiFillAmazonCircle />
+                        <p>Amazon</p>
+                    </div>
+                    <div style={{
+                        animationDelay: "0.7s",
+                    }}>
+                        <AiFillYoutube />
+                        <p>Youtube</p>
+                    </div>
+                    <div style={{
+                        animationDelay: "1s",
+                    }}>
+                        <AiFillInstagram />
+                        <p>Instagram</p>
+                    </div>
+                </article>
+            </div>
+        </>
+    )
 }
 
 export default Home
