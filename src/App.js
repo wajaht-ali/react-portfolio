829539e325e7c8ad7c551b536fd9609dfced76e2
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-07-14 11:46:30 +0500

    components testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h2>I'm Wajahat Ali</h2>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div >
+        <p>I'm from Home.</p>
+    </div>
+  )
+}
+
+export default Home
